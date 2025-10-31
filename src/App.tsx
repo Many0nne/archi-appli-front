ff0cd24108fc813c8b6b3cf7f67bef1863dca9d6
Many0nne <ptitesailes@gmail.com>
2025-10-31 10:16:01 +0100

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Button variant="contained">Hello world</Button>
+      <p className="text-red-500">test</p>
+    </div>
+  )
+}
+
+export default App
